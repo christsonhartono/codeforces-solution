c77e969f2e2967527ad45ef6a152d75cc757665f
christson <jhanacakro1@gmail.com>
2020-05-14 18:44:28 +0700

    C++

---
+++ Young Physicist.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n, suma=0, sumb=0, sumc=0;
+	cin>>n;
+	
+	for(int i=0; i<n; i++){
+		int a,b,c;
+		cin>>a>>b>>c;
+		
+		suma+=a;
+		sumb+=b;
+		sumc+=c;
+	}
+	
+	if(suma==0 && sumb==0 && sumc==0)
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
+		
+	return 0;
+}
