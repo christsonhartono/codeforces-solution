c7b0c2a3f7b93ba0c040ae9d4cded0fb78662a0b
christson <jhanacakro1@gmail.com>
2020-05-14 18:29:52 +0700

    C++

---
+++ Stones on the Table.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n, count=0;
+	string stones;
+	
+	cin>>n;
+	cin>>stones;
+	
+	for(int i=1; i<n; i++){
+		if(stones[i]==stones[i-1]){
+			stones[i] = stones[i-1];
+			count++;
+		}		
+	}
+	cout<<count;
+	
+	return 0;
+}
