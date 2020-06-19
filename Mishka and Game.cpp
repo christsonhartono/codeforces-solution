a484a86e4525afb55bfa8376e8c889414cdc4376
christson <jhanacakro1@gmail.com>
2020-06-19 13:58:39 +0700

    C++

---
+++ Mishka and Game.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,a,b,m=0,c=0;
+	cin>>n;
+	
+	for(int i=0; i<n; i++){
+		cin>>a>>b;
+		
+		if(a>b){
+			m++;
+		}else if(a<b){
+			c++;
+		}
+	}
+	
+	if(m>c){
+		cout<<"Mishka"<<endl;
+	}else if(m<c){
+		cout<<"Chris"<<endl;
+	}else{
+		cout<<"Friendship is magic!^^"<<endl;
+	}
+	return 0;
+}
