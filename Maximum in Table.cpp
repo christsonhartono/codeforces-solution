a8d3294bc010e59f8371b34070d10e9311f681f9
christson <jhanacakro1@gmail.com>
2020-06-20 18:34:07 +0700

    C++

---
+++ Maximum in Table.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int arr[10][10];
+
+int main(){
+	int n;
+	cin>>n;
+	
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			if(i==0){
+				arr[i][j] = 1;
+			}else{
+				if(j==0){
+					arr[i][j]+=arr[i-1][j];
+				}else{
+					arr[i][j]+=(arr[i-1][j]+arr[i][j-1]);
+				}
+			}
+		}
+	}	
+	cout<<arr[n-1][n-1]<<endl;
+	return 0;
+}
